335fb41811c7a6aec4badc1e14a24e7f9d644bb1
n <00004444@skiff.com>
2023-08-16 16:27:03 -0400

    pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+  let result = '';
+  if (count < 0) { return 'ERROR' }
 
+  for (let i = 0; i < count; i++) {
+    result += str;
+  }
+  return result
 };
 
 // Do not edit below this line
